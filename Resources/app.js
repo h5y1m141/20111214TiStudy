69a4dd1dbf85cc9806c3f6ee02cc35303cc3bbf5
hiroshi oyamada <h5y1m141+github@gmail.com>
2011-12-07 15:17:36 +0900

    Titanium.UI.createTableViewRowを使って、Row内部にLabelとImageViewを配置した少し複雑なレイアウトを作成

--- Resources/app.js
+++ Resources/app.js
@@ -8,11 +8,11 @@ var tabGroup = Titanium.UI.createTabGroup();
 //
 // create base UI tab and root window
 //
-var win1 = Titanium.UI.createWindow({  
+var win1 = Titanium.UI.createWindow({
     title:'Tab 1',
     backgroundColor:'#fff'
 });
-var tab1 = Titanium.UI.createTab({  
+var tab1 = Titanium.UI.createTab({
     icon:'KS_nav_views.png',
     title:'Tab 1',
     window:win1
@@ -34,33 +34,53 @@ win1.add(label1);
 //
 // create controls tab and root window
 //
-var win2 = Titanium.UI.createWindow({  
+var win2 = Titanium.UI.createWindow({
     title:'Tab 2',
     backgroundColor:'#fff'
 });
-var tab2 = Titanium.UI.createTab({  
+var tab2 = Titanium.UI.createTab({
     icon:'KS_nav_ui.png',
     title:'Tab 2',
     window:win2
 });
 
+var container = [];
+for(var i=0;i<9;i++){
+  var row = Titanium.UI.createTableViewRow({
+    height:80
+  });
 
-var tableViewData = [
-  {title:'Row1',hasChild:true},
-  {title:'Row2',hasDetail:true},
-  {title:'Row3',hasCheck:true}
-];
-var tableView = Titanium.UI.createTableView({
-	data:tableViewData
-})
+  var iconImage = Titanium.UI.createImageView({
+    left:5,
+    top:5,
+    width:50,
+    height:50,
+    image:'image/' + i + '.jpg'
+  });
+  row.add(iconImage);
+
+  var rowText = Titanium.UI.createLabel({
+    text:'これは' + i + '番目のRowとなるテキストです',
+    top:10,
+    left:60,
+    width:240,
+    height:'auto'
+  });
+  row.add(rowText);
+
+  container.push(row);
+}
+
+var tableView = Titanium.UI.createTableView();
+tableView.setData(container);
 win2.add(tableView);
 
 
 //
 //  add tabs
 //
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
+tabGroup.addTab(tab1);
+tabGroup.addTab(tab2);
 
 
 // open tab group
